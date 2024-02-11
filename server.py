4d39b3aca429c62613bb80bec41e07e3b2e80c99
add0794 <add0794@gmail.com>
2024-02-11 18:14:34 -0500

    debug templates and server

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
-import db_init
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Integer, String, Float
 # import sqlite3
 
 '''
@@ -18,9 +19,8 @@ This will install the packages from requirements.txt for this project.
 
 app = Flask(__name__)
 
-all_books = []
-
 # SQLite version
+
 # db = sqlite3.connect("books-collection.db") # creates the database
 # cursor = db.cursor() # creates the cursor
 # cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)") # creates the table
@@ -28,16 +28,23 @@ all_books = []
 # db.commit() # ensures that the entry is placed
 # db.close()
 
-# create the extension
-db = SQLAlchemy()
-# configure the SQLite database, relative to the app instance folder
+# Create the Flask application
+app = Flask(__name__)
+
+# Configure the SQLite database, relative to the app instance folder
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///new-books-collection.db'
-# initialize the app with the extension
+
+# Create the SQLAlchemy extension and specify the base class
+Base = declarative_base()
+db = SQLAlchemy(model_class=Base)
+
+# Initialize the Flask application with the SQLAlchemy extension
 db.init_app(app)
 
-
+# Define the Book model and map it to the database table
 class Book(db.Model):
-    # __tablename__ = 'books'
+    __tablename__ = 'new-books-collection'
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String, nullable=False)
     author = db.Column(db.String, nullable=False)
@@ -50,116 +57,66 @@ with app.app_context():
 
 @app.route("/")
 def home(name=None):
-    return render_template('index.html', books=all_books)
+    # Execute your query to get the result set
+    result_set = db.session.query(Book).all()
+
+    # Check if the result set is empty
+    is_empty = not result_set
+
+    # READ ALL RECORDS
+    # Construct a query to select from the database. Returns the rows in the database
+    result = db.session.execute(db.select(Book).order_by(Book.title))
+    # Use .scalars() to get the elements rather than entire rows from the database
+    all_books = result.scalars()
+    return render_template('index.html', books=all_books, is_empty=is_empty)
 
 
-@app.route("/add", methods=["POST", "GET"])
+@app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        title = request.form.get('title')
-        author = request.form.get('author')
-        rating = request.form.get('rating')
-        all_books.append({'title': title, 'author': author, 'rating': rating})
-        # new_book = {
-        #     "title": request.form["title"],
-        #     "author": request.form["author"],
-        #     "rating": request.form["rating"]
-        # }
-        # all_books.append(new_book)
-        return redirect(url_for("home"))
+        new_book = Book(
+            title=request.form["title"],
+            author=request.form["author"],
+            rating=request.form["rating"]
+        )
+        db.session.add(new_book)
+        db.session.commit()
+        return redirect(url_for('home'))
+
     return render_template('add.html')
 
 
-# @app.route("/edit", methods=["GET", "POST"])
-# def edit():
-#     # if request.method == "PUT":
-#     #     book_id = request.form["id"]
-#     #     book_to_update = db.get_or_404(Book, book_id)
-#     #     book_to_update.rating = request.form["rating"]
-#     #     db.session.commit()
-#     #     return redirect(url_for('home'))
-#     # return render_template('edit.html')
-#     id = request.args.get('id')
-#     book_selected = db.get_or_404(books, id)
-#     if request.method == 'POST':
-#         book_selected.rating = request.form['rating']
-#         db.session.commit()
-#         return redirect(url_for('home'))
-#     return render_template('edit.html', book=book_selected)
-
-# def edit():
-#     id = request.args.get('id')
-#     book_selected = books.query.get_or_404(id)
-
-#     if request.method == 'POST':
-#         book_selected.rating = request.form['rating']
-#         db.session.commit()
-#         return redirect(url_for('home'))
-
-#     return render_template('edit.html', book=book_selected)
-
-@app.route('/edit', methods=['GET', 'POST'])
-def edit(id):
-    the_book = Book.query.get(id)
- 
-    if request.method == 'POST':
-        the_book.rating = request.form['new-rating']
+@app.route('/edit', methods=["GET", "POST"])
+def edit():
+    if request.method == "POST":
+        book_id = request.form["id"]
+        book_to_update = db.get_or_404(Book, book_id)
+        # Update title if form input is not empty
+        if request.form["title"]:
+            book_to_update.title = request.form["title"]
+        # Update author if form input is not empty
+        if request.form["author"]:
+            book_to_update.author = request.form["author"]
+        # Update rating if form input is not empty
+        if request.form["rating"]:
+            book_to_update.author = request.form["rating"]
         db.session.commit()
         return redirect(url_for('home'))
- 
-    return render_template("edit_rating.html", book=the_book)
-
-# @app.route("/edit", methods=["GET", "POST"])
-# def edit():
-#     if request.method == "POST":
-#         #UPDATE RECORD
-#         book_id = request.form["id"]
-#         book_to_update = db.get_or_404(Book, book_id)
-#         book_to_update.rating = request.form["rating"]
-#         db.session.commit()
-#         return redirect(url_for('home'))
-#     book_id = request.args.get('id')
-#     book_selected = db.get_or_404(Book, book_id)
-#     return render_template("edit_rating.html", book=book_selected)
-    # book = Book.query.get_or_404()
-
-    # if request.method == "POST":
-    #     new_rating = request.form.get('rating')
-    #     if new_rating is not None:
-    #         # Update the book's rating in the database
-    #         book.rating = new_rating
-    #         db.session.commit()
-    #         return redirect(url_for('home'))
-
-    # return render_template('edit.html', book=book)
-
-        #     rating = request.form.get('rating')
-    #     # all_books.append({'title': title, 'author': author, 'rating': rating})
-    #     # new_book = {
-    #     #     "title": request.form["title"],
-    #     #     "author": request.form["author"],
-    #     #     "rating": request.form["rating"]
-    #     # }
-    #     # all_books.append(new_book)
-    #     return redirect(url_for("home"))
-    # return render_template('edit.html')
-
-    # book_id = request.form.get('title')
-    # if request.method == "PUT":
-    #     new_rating = request.form.get('rating')
-        # if new_rating is not None:
-        #     try:
-        #         all_books[index]["rating"] = int(new_rating)
-        #         return redirect(url_for("home"))
-        #     except IndexError:
-        #         return "Index out of range", 404
-        #     except ValueError:
-        #         return "Invalid rating value", 400
-        # else:
-        #     return "Missing 'rating' in form data", 400
+    book_id = request.args.get('id')
+    book_selected = db.get_or_404(Book, book_id)
+    return render_template("edit.html", book=book_selected)
 
 
-# @app.route("/delete", methods=["DELETE"])
+@app.route("/delete")
+def delete():
+    book_id = request.args.get('id')
+    # DELETE A RECORD BY ID
+    book_to_delete = db.get_or_404(Book, book_id)
+    # Alternative way to select the book to delete.
+    # book_to_delete = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 
 if __name__ == "__main__":
