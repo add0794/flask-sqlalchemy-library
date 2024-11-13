6d654d2700273401e4a119d2c5729a72a618d6d3
add0794 <add0794@gmail.com>
2024-11-13 14:59:42 -0500

    add method to clear database's tables

--- clear_tables.py
+++ clear_tables.py
@@ -2,16 +2,15 @@ from database_backend import database_path
 import os
 from server import db, Book
 
-def delete_database():
+def clear_tables():
     if os.path.exists(database_path()):
         # Delete all the records in the database
         db.reflect()
         db.drop_all()
-        # Delete the database file
-        os.remove(database_path())
-        print("Database deleted successfully!")
+        print("Tables & data deleted successfully!")
     else:
         print("No database found to delete.")
 
 # Call the delete_database() function to delete the database
-delete_database()
+clear_tables()
+
